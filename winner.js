13ee2f22b4a8a43001f5236c09b4f9294ef9794c
Franki Callard <francescalily@outlook.com>
2023-07-23 15:01:51 +0100

    adding winner function

---
+++ winner.js
@@ -0,0 +1,60 @@
+const prompt = require('prompt-sync')();
+const c = require('ansi-colors');
+
+
+function winner (userInput, computerInput) {
+
+
+    if (userInput === computerInput) {
+        console.log(c.bgYellowBright(`It's a draw. No points for either of you:-(`))
+    } else if ((userInput === 'rock' && computerInput === 'scissors') || (userInput === 'paper' && computerInput === 'rock') || (userInput === 'scissors' && computerInput === 'paper')) {
+      userScore++;
+        return (c.green(`You WIN! You now have ${userScore} points`));
+
+  } else {
+    computerScore++;
+    return (c.red(`The computer won:-(. The computer now has ${computerScore} points`))
+    
+  }
+}
+
+module.exports = {
+    winner
+};
+
+// const winner = (getUserInput, computerChoice) => {
+//     let userCount = 0;
+//     let computerCount = 0;
+// if (getUserInput === computerChoice) {
+// return 'It was a draw!'
+// }
+
+// if (computerChoice === 'rock' && getUserInput === 'paper') {
+//     userCount++;
+// return `You won! You now have ${userCount} points`;
+// }
+// if (computerChoice === 'rock' && getUserInput === 'scissors') {
+//     computerCount++;
+// return `The computer won! The computer now has ${computerCount} points`;
+// }
+
+// if (computerChoice === 'paper' && getUserInput === 'rock') {
+//     computerCount++;
+//     return `The computer won! The computer now has ${computerCount} points`;
+// }
+
+// if (computerChoice === 'paper' && getUserInput === 'scissors') {
+//     userCount++;
+//     return `You won! You now have ${userCount} points`;
+// }
+
+// if (computerChoice === 'scissors' && getUserInput === 'rock') {
+//     userCount++;
+//     return `You won! You now have ${userCount} points`;
+// }
+
+// if (computerChoice === 'scissors' && getUserInput === 'paper') {
+//     computerCount++;
+//     return `The computer won! The computer now has ${computerCount} points`;
+// }
+// }
